338e8f48f73c62bfcb3bdd060f8aa49deeb64284
johnzeng <johnzeng@fareastone.com.tw>
2021-08-17 11:56:05 +0800

    set eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: ['plugin:vue/recommended', 'eslint:recommended'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 'off',
+    'eol-last': ['error', 'never'],
+    "vue/max-attributes-per-line": [2, {
+      "singleline": 10,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
+      }
+    }],
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/multiline-html-element-content-newline":"off",
+    "vue/name-property-casing": ["error", "PascalCase"],
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+}
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
+'use strict'
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    // name: name,
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]

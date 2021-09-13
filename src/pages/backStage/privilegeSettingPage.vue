8e2214f7876262eaed997ab88a8a733e145f5db1
ellie0921 <89962784+ellie0921@users.noreply.github.com>
2021-09-13 13:53:41 +0800

    [DEV][TPES]權限設定頁prototype調整
    1.權限設定頁prototype調整

--- src/pages/backStage/privilegeSettingPage.vue
+++ src/pages/backStage/privilegeSettingPage.vue
@@ -1,27 +1,15 @@
 <template>
   <v-container>
-
     <!-- 頁面上半 第一頁-->
-
     <div>
-      <v-row>
-        <v-col class="ml-10">
-          <div class="cal-btn-bar">
-            <!-- 標題 -->
-            <div class="content-title font-bold font-40px">
-              權限設定
-            </div>
-          </div>
-        </v-col>
-      </v-row>
+      <h2 class="font-bold">權限設定</h2>       
     </div>
 
-    <div class="mt-15">
-      <v-row>
-        <v-col cols="1" />
+    <div class="ml-10">
+      <v-row class="mt-5" align="center">
         <v-col cols="1">
           <div>
-            <span class="font-bold font-26px">角色</span>
+            <span class="font-bold font-18px">角色</span>
           </div>
         </v-col>
         <v-col
@@ -37,17 +25,16 @@
             hide-details
             dense
             placeholder="請選擇角色"
-            class="font-bold font-20px"
+            class="font-bold font-18px"
             @change="chooseDivision()"
           />
           
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="1" />
         <v-col cols="1">
           <div>
-            <span class="font-bold font-26px">說明</span>
+            <span class="font-bold font-18px">說明</span>
           </div>
         </v-col>
         <v-col
@@ -68,10 +55,9 @@
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="1" />
         <v-col cols="1">
           <div>
-            <span class="font-bold font-26px">權限</span>
+            <span class="font-bold font-18px">權限</span>
           </div>
         </v-col>
         <v-col
@@ -89,7 +75,7 @@
               inset
             >
               <v-expansion-panel-header class="headerLine">
-                <span class="d-inline-block mr-5 mt-5 font-bold font-26px">{{ item.text }}</span>      
+                <span class="d-inline-block mr-5 mt-5 font-bold font-18px">{{ item.text }}</span>      
                
                
               </v-expansion-panel-header>
@@ -107,7 +93,7 @@
                       color="primary"
                       @click="turnSubAuth(item,items)"
                     />
-                    <span class="d-inline-flex font-bold font-20px">{{ items.type == 1 ? '無紙化-' : null }}{{ items.text }}</span>
+                    <span class="d-inline-flex font-bold font-18px">{{ items.type == 1 ? '無紙化-' : null }}{{ items.text }}</span>
                   </v-col>                      
                 </v-row>
               </v-expansion-panel-content>
@@ -126,38 +112,3 @@
   </v-container>
 </template>
 <script src="./privilegeSettingPage.js"></script>
-<!--CSS暫放區 -->
-<style>
-  .my-textarea textarea { 
-    color: black !important;
-    font-weight:bold;
-    font-size:20px !important;
-  }
-  .v-select {
-    font-size: 20px;
-}
-  .headerLine{
-    border-bottom:black 1px solid;
-    padding: 8px 0px;
-    margin: 10px 0px;
-    min-height: 0;
-    font-size:40px;
-    font-weight: bold
-  }
-  .font-bold{
-    font-weight:bold
-  }
-  .font-40px{
-    font-size:40px
-  }
-   .font-26px{
-    font-size:26px
-  }
-  .font-20px{
-    font-size:20px
-  }
-  .d-end{
-    display: flex;
-    justify-content: flex-end;
-  }
-</style>

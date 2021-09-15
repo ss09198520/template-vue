02d2d077a0cc4a52e925b6b92ad89c03828075e1
ellie0921 <89962784+ellie0921@users.noreply.github.com>
2021-09-15 13:35:43 +0800

    [DEV][TPES] 我的調閱區頁面 補上瀏覽案件的跳出視窗
    1.我的調閱區頁面 補上瀏覽案件的跳出視窗

--- src/pages/my/myReadPage.js
+++ src/pages/my/myReadPage.js
@@ -1,6 +1,11 @@
 
+import MessageService from "@/assets/services/message.service";
+import formPage from "../FormPage/FormPage.vue";
 
 export default{
+    components: {
+        formPage,
+    },
     data(){
         return{
             //是否顯示下方清單
@@ -27,9 +32,36 @@ export default{
                 { mani: true, orderId: 'A00605', orderDate: '20210910 09:45', fileInDate:'20210919 11:05', inquireStatus:'有效日至 20211030', orderType:'APR0160', orderItems:'F3030表燈非時間電價停用廢止'},
                 { mani: false, orderId: 'A00619', orderDate: '20210910 13:44', fileInDate:'20210919 11:05', inquireStatus:'申請中', orderType:'APR0200', orderItems:'I0510故障換表'},
             ],
+
+            browserModel: false,
+            returnReason: null,
+            returnReasonModel: false,
         }
     },
     methods: {
-        
+        browerOrder(item){
+            console.log(item);
+            this.browserModel = true;
+        },
+        returnSubmit(){
+            //刪除該筆資料
+            if (this.selectIndex > -1) {
+                this.empMockList.splice(this.selectIndex, 1);
+              }
+            this.returnReasonModel = false;
+            this.browserModel = false;
+            MessageService.showSuccess("退件成功✓");
+        },
+        checkSubmit(){
+            //刪除該筆資料
+            if (this.selectIndex > -1) {
+                this.empMockList.splice(this.selectIndex, 1);
+              }
+            MessageService.showSuccess("核算成功✓");
+            this.browserModel = false;
+        },
+        retrunOrder(){
+            this.returnReasonModel = true;
+        },
     },
 }
--- src/pages/my/myReadPage.vue
+++ src/pages/my/myReadPage.vue
@@ -64,7 +64,7 @@
                 <div v-if="item.mani==true">
                   <v-btn
                     color="primary"
-                    @click="search(item)"
+                    @click="browerOrder(item)"
                   >
                     瀏覽案件
                   </v-btn>                 
@@ -95,6 +95,109 @@
           />
         </div>
       </div>
+      <!-- 瀏覽案件 -->
+      <v-dialog
+        v-model="browserModel"
+        max-width="1200px"
+      >
+        <v-card>
+          <v-card-title class="text-h5 lighten-2" style="background-color:#363636; color:white;">          
+            瀏覽案件
+            <v-spacer />
+            <v-btn
+              color="white"
+              icon
+              small
+              text
+              @click="browserModel = false"
+            >
+              <v-icon> mdi-close </v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-card-text>
+            <form-page restrict-mode="audit" />
+          </v-card-text>
+          <v-card-actions class="d-end mt-5">
+            <v-btn              
+              color="error"            
+              @click="retrunOrder()"
+            >
+              &emsp;退件&emsp;
+            </v-btn>
+            <v-btn              
+              color="success"            
+              @click="checkSubmit()"
+            >
+              核算通過
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog
+        v-model="returnReasonModel"
+        max-width="600"
+      >
+        <v-card>
+          <v-card-title class="text-h5 lighten-2" style="background-color:#363636; color:white;">          
+            請填寫退件原因
+            <v-spacer />
+            <v-btn
+              color="white"
+              icon
+              small
+              text
+              @click="returnReasonModel = false"
+            >
+              <v-icon> mdi-close </v-icon>
+            </v-btn>
+          </v-card-title>
+          <v-card-text class="font-18px">
+          
+            <v-row class="mt-6 ml-1" align="center">
+              <v-col cols="3">
+                退件原因
+              </v-col>
+              <v-col cols="7">
+                <v-text-field
+                  outlined
+                  hide-details                                         
+                  dense
+                  placeholder="請輸入退件原因"
+                />
+              </v-col>
+            </v-row>
+            <v-row class="ml-1" align="center">
+              <v-col cols="3">
+                退件說明
+              </v-col>
+              <v-col cols="7">
+                <v-textarea            
+                  color="#ADADAD"
+                  outlined
+                  name="input-7-4"
+                  :no-resize="true"
+                  placeholder="請輸入退件說明"
+                  hide-details
+                />
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-actions class="d-end mt-5">
+            <v-btn              
+              color="normal"            
+              @click="returnReasonModel = false"
+            >
+              &emsp;取消&emsp;
+            </v-btn>
+            <v-btn              
+              color="error"            
+              @click="returnSubmit()"
+            >
+              確認退件
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-container>
   </v-app>
 </template>

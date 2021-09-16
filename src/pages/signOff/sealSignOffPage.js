dec98db7c4b203d2f526a05555422a677d50fe3e
ellie0921 <89962784+ellie0921@users.noreply.github.com>
2021-09-16 18:14:24 +0800

    [DEV][TPES] 專用章簽核prototype 調整
    1.專用章簽核prototype 調整

--- src/pages/signOff/sealSignOffPage.js
+++ src/pages/signOff/sealSignOffPage.js
@@ -10,18 +10,20 @@ export default {
              //預設簽核顯示範圍按鈕
              displayAll: true,
              sealSignListHeaders:[
-                { text: '調閱狀態', value: 'inquireStatus', align: 'center' },
+                { text: '狀態', value: 'inquireStatus', align: 'center' },
                 { text: '受理日期', value: 'orderDate', align: 'center' },
                 { text: '受理編號', value: 'orderId', align: 'center' },
                 { text: '電號', value: 'electNum', align: 'center' },
                 { text: '登記單種類', value: 'orderType', align: 'center' },
-                { text: '受理項目', value: 'orderItems', align: 'center' },                
+                { text: '受理項目', value: 'orderItems', align: 'center' },
+                { text: '專用章檔案下載', value: 'download', align: 'center' },    
                 { text: '狀態操作', value: 'mani', align: 'center' }
              ],
              sealSignList:[
                  {status:false,inquireStatus:'待簽核',orderId:'A00349',orderDate: '20210910 10:00',electNum:'7140000123',orderType:'APR0370',orderItems:'QA210軍眷用電申請優待'},
                  {status:false,inquireStatus:'待簽核',orderId:'A00389',orderDate: '20210908 10:00',electNum:'7140000128',orderType:'APR0200',orderItems:'I0510故障換表'},
-             ],
+                 {status:true,inquireStatus:'套印完成',orderId:'A00389',orderDate: '20210908 10:00',electNum:'7140000128',orderType:'APR0200',orderItems:'I0510故障換表'},
+                ],
              sealSignListPageCount:0,
              sealSignListPage:1,
         }
@@ -30,7 +32,7 @@ export default {
         sign(item){
             console.log(item);
             let index = this.sealSignList.indexOf(item);
-            this.sealSignList[index].inquireStatus = '已簽核';
+            this.sealSignList[index].inquireStatus = '套印完成';
             this.sealSignList[index].status = true;
             MessageService.showSuccess("簽核成功✓");
         }

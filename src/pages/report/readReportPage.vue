95e4144bbbd0b3570f4e34da915049e0e7a52285
AnthonyChi <anthony.chi@hy-tech.com.tw>
2021-09-17 10:48:30 +0800

    [DEV][TPES] 1. 報表區頁面將 js 與 vue 拆開為不同檔案 2. 調閱清單移除調閱管理員簽核時間欄位(因為調閱管理員就是申請人本人，應該是不用簽核)

--- src/pages/report/readReportPage.vue
+++ src/pages/report/readReportPage.vue
@@ -126,10 +126,6 @@
               <span v-if="!item.signOffDate2" style="color: gray;">未簽核</span>
               <span v-else>{{ item.signOffDate2 }}</span>
             </template>
-            <template v-slot:item.signOffDate3="{ item }">
-              <span v-if="!item.signOffDate3" style="color: gray;">未簽核</span>
-              <span v-else>{{ item.signOffDate3 }}</span>
-            </template>    
             <template v-slot:item.download="{ item }">
               <v-tooltip top>
                 <template v-slot:activator="{ on }">
@@ -162,39 +158,4 @@
   </v-app>
 </template>
 
-<script>
-
-export default {
-    name: 'ReadReport',
-    props: {
-    
-    },
-    data() {
-        return {
-          startDate: false,
-          endDate: false,
-          monthPicker: false,
-          headers: [
-            { text: '調閱月份', value: 'readMonth', align: 'center' },
-            { text: '區處', value: 'region', align: 'center' },
-            { text: '調閱管理員簽核時間', value: 'signOffDate1', align: 'center' },
-            { text: '核算課長簽核時間', value: 'signOffDate2', align: 'center' },  
-            { text: '電費經理簽核時間', value: 'signOffDate3', align: 'center' },          
-            { text: '下載', value: 'download', align: 'center' },
-            { text: '簽核', value: 'signOff', align: 'center' }
-          ],
-          itemList: [
-            {signOff: false, readMonth: '2021/08', region: '台中', signOffDate1: '2021/09/01 13:00:26', signOffDate2: '2021/09/02 10:36:53', signOffDate3: '2021/09/02 14:42:51', download: true},
-            {signOff: true, readMonth: '2021/09', region: '台中', signOffDate1: '2021/10/01 14:14:42', signOffDate2: '', signOffDate3: '', download: true}
-          ],
-          dataListPage: 1,
-          dataListPageCount: 1,
-          searchText: '　查詢　',
-          signOffText: '　簽核　'
-        }
-    },
-    methods: {
-
-    }
-}
-</script>
+<script src="./readReportPage.js" />
--- src/pages/report/returnReportPage.vue
+++ src/pages/report/returnReportPage.vue
@@ -158,40 +158,4 @@
   </v-app>
 </template>
 
-<script>
-
-export default {
-    name: 'ReturnReport',
-    props: {
-    
-    },
-    data: () => ({
-        startDate: false,
-        endDate: false,
-        headers: [
-          { text: '退件日期', value: 'returnDate', align: 'center' },
-            { text: '區處', value: 'region', align: 'center' },
-            { text: '主辦業務計劃員簽核時間', value: 'signOffDate1', align: 'center' },
-            { text: '核算課長簽核時間', value: 'signOffDate2', align: 'center' },         
-            { text: '下載', value: 'download', align: 'center' },
-            { text: '簽核', value: 'signOff', align: 'center' }
-        ],
-        itemList: [
-            {signOff: false, returnDate: '2021/09/06 ~ 2021/09/12', region: '台中', signOffDate1: '2021/09/17 13:00:26', signOffDate2: '2021/09/18 10:36:53', download: true},
-            {signOff: true, returnDate: '2021/09/13 ~ 2021/09/19', region: '台中', signOffDate1: '', signOffDate2: '', download: true}
-        ],
-        dataListPage: 1,
-        dataListPageCount: 1,
-        searchText: '　查詢　',
-        signOffText: '　簽核　'
-    }),
-    mounted() {
-
-    },
-    methods: {
-        search(){
-
-        }
-    }
-}
-</script>
+<script src="./returnReportPage.js" />

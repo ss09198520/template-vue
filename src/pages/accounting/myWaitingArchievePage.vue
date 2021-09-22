6497c84ac3bb17339a332d88e1e3c838902952d7
ellie0921 <89962784+ellie0921@users.noreply.github.com>
2021-09-22 11:24:58 +0800

    [DEV][TPES] 待歸檔prototype 調整
    1.增加資料篩選條件
    2.新增欄位:電號、整理號碼、戶名
    3.移除欄位：待歸檔日期、登記單種類

--- src/pages/accounting/myWaitingArchievePage.vue
+++ src/pages/accounting/myWaitingArchievePage.vue
@@ -11,90 +11,98 @@
           </div>
         </div>
       </div>
-      <div style="margin-bottom: 10px;">        
-        <v-tooltip top>
-          <template v-slot:activator="{ on }">
-            <v-btn
-              :class="showAllArchieve? 'primary' : ''"                                       
-              class="ma-2"
-              fab
-              small                
-              v-on="on"
-              @click="showArchieve('all')"
-            >
-              <v-icon>mdi-text-box-multiple-outline</v-icon>
-            </v-btn>
-          </template>
-          <span>顯示全部</span>
-        </v-tooltip>        
-        <v-tooltip top>
-          <template v-slot:activator="{ on }">
-            <v-btn
-              :class="showMyArchieve? 'primary' : ''"                                       
-              class="ma-2"
-              fab
-              small                
-              v-on="on"
-              @click="showArchieve('my')"
-            >
-              <v-icon>mdi-text-box-search-outline</v-icon>
-            </v-btn>
-          </template>
-          <span>只顯示我的待歸檔</span>
-        </v-tooltip>
-      </div>
-      <hr>
-      <v-row>
-        <v-col cols="12">    
-          <v-data-table
-            :headers="headers"
-            :items="itemList"
-            :page.sync="dataListPage"
-            :items-per-page="10"
-            hide-default-footer
-            no-data-text="查無資料"
-            class="elevation-1"
-            disable-sort
-            @page-count="dataListPageCount = $event"
-          />
-          <!-- 選頁 -->
-          <div class="mt-2">
-            <v-pagination
-              v-model="dataListPage"
-              color="#2F59C4"
-              :length="dataListPageCount"
+
+      <div class="mt-10 ml-10 font-18px font-bold">
+        <v-row align="center">
+          <v-col cols="1">
+            受理號碼     
+          </v-col>          
+          <v-col
+            cols="3"
+          >
+            <v-text-field
+              outlined
+              hide-details                                         
+              dense
+              placeholder="請輸入受理編號"
             />
-          </div>
-        </v-col>
-      </v-row>
+          </v-col>
+          <v-col cols="1" />
+          <v-col cols="1">
+            電號
+          </v-col>                                             
+          <v-col
+            cols="3"
+          >
+            <v-text-field                           
+              outlined
+              hide-details
+              dense
+              placeholder="請輸入電號"
+            />
+          </v-col>     
+        </v-row>
+        <v-row align="center">
+          <v-col cols="1">
+            整理號碼     
+          </v-col>          
+          <v-col
+            cols="3"
+          >
+            <v-text-field
+              outlined
+              hide-details                                         
+              dense
+              placeholder="請輸入整理編號"
+            />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="11" />
+          <v-col>
+            <!-- <v-btn class="primary mt-5" @click="displayList = true">&emsp;查詢&emsp;</v-btn>     -->
+            <v-tooltip top>
+              <template v-slot:activator="{ on }">
+                <v-btn
+                  class="ma-2"
+                  fab
+                  small
+                  color="primary"
+                  v-on="on"
+                  @click="hasShowList = true"
+                >
+                  <v-icon v-text="'mdi-magnify'" />
+                </v-btn>
+              </template>
+              <span>查詢</span>
+            </v-tooltip>
+          </v-col>
+        </v-row>  
+        <hr class="mt-10 mb-10">
+      </div>
+      <div v-if="hasShowList" class="ml-10">    
+        <v-data-table
+          :headers="headers"
+          :items="itemList"
+          :page.sync="dataListPage"
+          :items-per-page="10"
+          hide-default-footer
+          no-data-text="查無資料"
+          class="elevation-1"
+          disable-sort
+          @page-count="dataListPageCount = $event"
+        />
+        <!-- 選頁 -->
+        <div class="mt-2">
+          <v-pagination
+            v-model="dataListPage"
+            color="#2F59C4"
+            :length="dataListPageCount"
+          />
+        </div>
+      </div>
     </v-container>
   </v-app>
 </template>
 
 <script src="./myWaitingArchievePage.js" />
-
-<style scoped>
-    .block {
-        height: 250px;
-        padding: 18px;
-        position: relative;
-    }
-
-    .block .block-content {
-        font-size: 24px;
-        font-weight: 700;
-        height: 100px;
-        white-space: pre-wrap;
-    }
-
-    .block .block-title {
-        font-size: 36px;
-        font-weight: bold;
-    }
-
-    .block .block-number {
-        font-weight: bold;
-        font-size: 96px;
-        margin-right: 20px;
-    }
-</style>

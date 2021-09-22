0011182681a465f2080a14300b14c9b03348b494
ellie0921 <89962784+ellie0921@users.noreply.github.com>
2021-09-22 14:07:53 +0800

    [DEV][TPES] 我的表單prototype 調整
    1.新增欄位:電號、計算日、戶名、契約種類
    2.刪除欄位：登記單種類
    3.移除取消案件按鈕

--- src/pages/my/myFormPage.js
+++ src/pages/my/myFormPage.js
@@ -19,23 +19,25 @@ export default{
             //預設總頁數
             orderListPageCount: 0,
             empListHeaders: [
-                { text: '受理編號', value: 'orderId', align: 'center' },
+                { text: '受理號碼', value: 'orderId', align: 'center' },
+                { text: '戶名', value: 'accntName', align: 'center' },
+                { text: '電號', value: 'eletricNo', align: 'center' },
+                { text: '契約種類', value: 'contractType', align: 'center' },
                 { text: '受理日期', value: 'orderDate', align: 'center' },
-                { text: '訂單狀態', value: 'orderStatus', align: 'center' },
-                { text: '登記單種類', value: 'orderType', align: 'center' },            
+                { text: '計算日', value: 'calDate', align: 'center' },
+                { text: '案件狀態', value: 'orderStatus', align: 'center' },     
                 { text: '受理項目', value: 'orderItems', align: 'center' },
                 { text: '代理案件', value: 'proxyEvent', align: 'center' },
                 { text: '代理件所有人', value: 'eventAgent', align: 'center' },
                 { text: '狀態操作', value: 'mani', align: 'center' }
             ],
             empMockList: [                
-                { mani: true, orderId: 'A00024', orderDate: '20210910 10:00', orderStatus:'受理中', orderType:'APR0370', orderItems:'QA210軍眷用電申請優待', proxyEvent:true,eventAgent:'王大明'},
-                { mani: false, orderId: 'A00615', orderDate: '20210909 11:21', orderStatus:'核算中', orderType:'APR0200', orderItems:'I0510故障換表', proxyEvent:'',eventAgent:''},
-                { mani: false, orderId: 'A00040', orderDate: '20210907 15:36', orderStatus:'核算中', orderType:'APR0200', orderItems:'I0520增加電表', proxyEvent:'',eventAgent:''},
-                { mani: true, orderId: 'A00605', orderDate: '20210910 09:45', orderStatus:'受理中', orderType:'APR0160', orderItems:'F3030表燈非時間電價停用廢止', proxyEvent:'',eventAgent:''},
-                { mani: false, orderId: 'A00619', orderDate: '20210910 13:44', orderStatus:'核算中', orderType:'APR0200', orderItems:'I0510故障換表', proxyEvent:'',eventAgent:''},
+                { mani: true, orderId: 'A00024',accntName: '劉艷艷',eletricNo:'000123',contractType:'包制',calDate:'20210909 11:21', orderDate: '20210910 10:00', orderStatus:'受理中', orderType:'APR0370', orderItems:'QA210軍眷用電申請優待', proxyEvent:true,eventAgent:'王大明'},
+                { mani: false, orderId: 'A00615',accntName: '劉齊民',eletricNo:'000123',contractType:'包制',calDate:'20210909 11:21', orderDate: '20210909 11:21', orderStatus:'核算中', orderType:'APR0200', orderItems:'I0510故障換表', proxyEvent:'',eventAgent:''},
+                { mani: false, orderId: 'A00040',accntName: '陳艷均',eletricNo:'000123',contractType:'包制',calDate:'20210909 11:21', orderDate: '20210907 15:36', orderStatus:'核算中', orderType:'APR0200', orderItems:'I0520增加電表', proxyEvent:'',eventAgent:''},
+                { mani: true, orderId: 'A00605',accntName: '王筱涵',eletricNo:'000123',contractType:'包制',calDate:'20210909 11:21', orderDate: '20210910 09:45', orderStatus:'受理中', orderType:'APR0160', orderItems:'F3030表燈非時間電價停用廢止', proxyEvent:'',eventAgent:''},
+                { mani: false, orderId: 'A00619',accntName: '連文彥',eletricNo:'000123',contractType:'包制',calDate:'20210909 11:21', orderDate: '20210910 13:44', orderStatus:'核算中', orderType:'APR0200', orderItems:'I0510故障換表', proxyEvent:'',eventAgent:''},
             ],
-            deleteOrderModel:false,
             selectIndex: null,
             browserModel: false, // 瀏覽案件視窗開關
             supplementModel: false, // 補件操作視窗開關
@@ -64,9 +66,7 @@ export default{
         action(type,item){
             // 抓出選的是第幾筆
             this.selectIndex = this.empMockList.indexOf(item);            
-            if(type=='delete'){
-                this.deleteOrderModel = true;
-            } else if(type == 'browse'){
+            if(type == 'browse'){
                 this.browserModel = true;
             } else if(type == 'supplement'){
                 this.supplementModel = true;

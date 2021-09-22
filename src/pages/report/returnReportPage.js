8bb5d2e6a5689d155649877f73e2344cd4aa59f6
ellie0921 <89962784+ellie0921@users.noreply.github.com>
2021-09-22 11:04:30 +0800

    [DEV][TPES] 退件報表prototype 調整
    1.移除不需要的欄位：區處

--- src/pages/report/returnReportPage.js
+++ src/pages/report/returnReportPage.js
@@ -8,15 +8,14 @@ export default {
         endDate: false,
         headers: [
           { text: '退件日期', value: 'returnDate', align: 'center' },
-            { text: '區處', value: 'region', align: 'center' },
             { text: '主辦業務計劃員簽核時間', value: 'signOffDate1', align: 'center' },
             { text: '核算課長簽核時間', value: 'signOffDate2', align: 'center' },         
             { text: '下載', value: 'download', align: 'center' },
             { text: '簽核', value: 'signOff', align: 'center' }
         ],
         itemList: [
-            {signOff: false, returnDate: '2021/09/06 ~ 2021/09/12', region: '台中', signOffDate1: '2021/09/17 13:00:26', signOffDate2: '2021/09/18 10:36:53', download: true},
-            {signOff: true, returnDate: '2021/09/13 ~ 2021/09/19', region: '台中', signOffDate1: '', signOffDate2: '', download: true}
+            {signOff: false, returnDate: '2021/09/06 ~ 2021/09/12',  signOffDate1: '2021/09/17 13:00:26', signOffDate2: '2021/09/18 10:36:53', download: true},
+            {signOff: true, returnDate: '2021/09/13 ~ 2021/09/19',  signOffDate1: '', signOffDate2: '', download: true}
         ],
         dataListPage: 1,
         dataListPageCount: 1,

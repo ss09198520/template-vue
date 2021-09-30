2781a57e80563c10cdfff72e2208e6b58845595a
ellie0921 <89962784+ellie0921@users.noreply.github.com>
2021-09-30 15:31:00 +0800

    [DEV][TPES]共用驗證validateUtil 新增驗證
    1.新增：電號驗證、英文或中文格式驗證、日期格式驗證、日期時間格式驗證

--- src/assets/services/validateUtil.js
+++ src/assets/services/validateUtil.js
@@ -13,6 +13,39 @@ const ValidateUtil = {
         return numsPattern.test(nums);
     },
 
+     /*
+    驗證傳入電號是否為全數字11碼
+    */
+    validateEletricNums (nums) {
+        const numsPattern = /^[0-9\uFF10-\uFF19]{11}$/;
+        return numsPattern.test(nums);
+    },
+
+     /*
+    驗證傳入字串是否為英文或中文
+    */
+   validateName(name){
+    const namePattern = /^[\u4e00-\u9fa5_a-zA-Z]+$/;
+    return namePattern.test(name);
+   },
+
+   /*
+    驗證傳入日期格式是否正確 2021-09-10
+    */
+    validateDate(date){
+        const namePattern = /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/;
+        return namePattern.test(date);
+    },
+
+
+    /*
+    驗證傳入日期時間格式是否正確 2021-09-10 08:00:00
+    */
+    validateDateTime(dateTime){
+        const namePattern = /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s+(20|21|22|23|[0-1]\d):[0-5]\d:[0-5]\d$/;
+        return namePattern.test(dateTime);
+    },
+
 
     /*
     驗證傳入參數是否為空(null、空字串、空陣列)

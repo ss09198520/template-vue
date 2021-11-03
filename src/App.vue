3cfe694309355682bbd846c811ff612788f52d60
HYTLuke <89962595+HYTLuke@users.noreply.github.com>
2021-11-03 17:27:08 +0800

    [DEV][TPES] Ajax Loading
    1. 新增 Loading 畫面

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,40 @@
       :max="10"
       animation-name="v-fade-right"
     />
+
+    <!-- loading -->
+    <v-overlay :value="isLoading" z-index="5000">
+      <v-progress-circular indeterminate size="64" />
+    </v-overlay>
   </v-app>
 </template>
 
+<script>
+import EventBus from '@/assets/services/eventBus.js';
+
+export default {
+  components: {
+
+  },
+  data: () => ({
+    isLoading: false, // 開啟loading畫面
+  }),
+  beforeMount() {
+    this.init();
+  },
+  methods: {
+    init() {
+      // 註冊啟動畫面loading監聽事件 (ajax)
+      EventBus.subscriber('toggleLoading',this.toggleLoading);
+    },
+    // 開啟Loading畫面
+    toggleLoading(isLoading){
+      this.isLoading = isLoading;
+    },
+  }
+}
+
+</script>
 
 <style lang="scss">
 @import '@/scss/overrides.scss';

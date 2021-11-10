5c8eaf7152ef237449702c26d9999131635cb12c
AnthonyChi <anthony.chi@hy-tech.com.tw>
2021-11-10 14:57:02 +0800

    [DEV][TPES] 修正下載專用章文件即使成功了還是會跳錯誤訊息

--- src/pages/signOff/sealSignOffPage.js
+++ src/pages/signOff/sealSignOffPage.js
@@ -117,7 +117,6 @@ export default {
                     }
                 },
                 (error) => {
-                    MessageService.showSystemError();
                     console.log(error);
                 }
             );

57d4d658187c92d191c1c40f2ac9834bc120148c
johnzeng <johnzeng@fareastone.com.tw>
2021-12-03 16:57:55 +0800

    移除vue router 舊寫法

--- src/permission.js
+++ src/permission.js
@@ -39,8 +39,10 @@ router.beforeEach(async(to, from, next) => {
           const accessRoutes = await store.dispatch('permission/generateRoutes', roles)// 請看src/store/modules/permission.js 內 generateRoutes
           
           // dynamically add accessible routes
-          // router.addRoute(...accessRoutes)
-          router.addRoutes(accessRoutes)
+          // router.addRoutes(accessRoutes) //old version
+          accessRoutes.forEach((route) => {
+            router.addRoute(route)
+          })
           
           // hack method to ensure that addRoutes is complete
           // set the replace: true, so the navigation will not leave a history record

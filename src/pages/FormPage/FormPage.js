712945b2cd94a8886071746c3f8fc68e7b4da4e8
Luke Lin <luke.lin@hy-tech.com.tw>
2021-12-27 18:59:38 +0800

    [DEV][TPES] TPES Form
    1. 移除關閉視窗

--- src/pages/FormPage/FormPage.js
+++ src/pages/FormPage/FormPage.js
@@ -1397,8 +1397,6 @@ export default {
           } else {
             MessageService.showInfo('客戶放棄問卷填寫','問卷調查')
           }
-          // 三秒後自動關閉頁面
-          setTimeout(() => window.close(), 3000);
         },
         closePortal() {
           if (this.windowRef) {
